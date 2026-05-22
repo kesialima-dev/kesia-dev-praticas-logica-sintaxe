043ff2303eb9d5e770decc87f4c520bd42c12162
Késia Lima <kesialimak0637@gmail.com>
2026-05-21 22:11:13 -0300

    exercicos de WHILE

---
+++ cronometro.py
@@ -0,0 +1,10 @@
+contador = 10
+
+while contador >= 0:
+    print(contador)
+    contador -= 1
+
+print("Bum💣💥")
+
+
+
---
+++ while.py
@@ -0,0 +1,26 @@
+#CRIAR A VARIÁVEL
+
+senha_usuario = input("Digite a senha:")
+
+#MONTAR A CONDIÇÃO
+
+while senha_usuario != "1234":
+    print("Senha incorreta, tente novamente!")
+    senha_usuario = input("Digite a senha")
+else:
+    print("Acesso concedido")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
